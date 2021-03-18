d77489ff7b9d67106edb2ee0ddf56dd4f7c7dc8c
Alicia Kmak <alicia.kmak@gmail.com>
2021-03-18 19:50:21 +0100

    random obstacles raising and falling ok

--- js/game.js
+++ js/game.js
@@ -1,61 +1,68 @@
 class Game {
-   constructor(ctx) { 
-    this.ctx = ctx
-    this.chihuahua = new chihuahua(ctx)
-    this.obstacle = new obstacle(ctx)
-    this.randomObstacles = []
-    this.bag = new bag(ctx)
-   }
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.chihuahua = new chihuahua(ctx);
+    // this.obstacle = new obstacle(ctx)
 
-   start() {
-      
-      this.assignControlToKeys()
-      
-      window.requestAnimationFrame(this.update.bind(this))
-   }
-   clear() {
-      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
-    }
+    this.randomObstacles = [
+      new obstacle(ctx),
+      new obstacle(ctx),
+      new obstacle(ctx),
+    ];
+    this.bag = new bag(ctx);
+  }
 
-   update() {
-      this.clear();
-      this.chihuahua.drawChihuahua();
-      this.bag.drawBag();
-      this.obstacle.drawObstacle();
-      this.obstacle.obstacleRandomFall();
-      //this.obstacle.createRandomObstacles();
-      
-      //this.chihuahua.collisionWithBag(chihuahua, bag);
-     // this.chihuahua.collisionWithObstacle(chihuahua, obstacle);
-      window.requestAnimationFrame(this.update.bind(this))
+  start() {
+    this.assignControlToKeys();
+    this.randomObstacles.forEach((obstacle) => {
+      obstacle.startMove();
+    });
+    window.requestAnimationFrame(this.update.bind(this));
+  }
+  clear() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
 
-   }
+  drawAllObstacle() {
+    this.randomObstacles.forEach((obstacle) => {
+      obstacle.drawObstacle();
+    });
+  }
 
-   assignControlToKeys() {
-      document.addEventListener('keydown', (event) => {
+  update() {
+    this.clear();
+    this.chihuahua.drawChihuahua();
+    this.bag.drawBag();
+    this.drawAllObstacle();
+    // this.obstacle.drawObstacle();
+    // this.obstacle.obstacleRandomFall();
+    // this.obstacle.createRandomObstacles();
+
+    //this.chihuahua.collisionWithBag(chihuahua, bag);
+    // this.chihuahua.collisionWithObstacle(chihuahua, obstacle);
+    window.requestAnimationFrame(this.update.bind(this));
+  }
+
+  assignControlToKeys() {
+    document.addEventListener("keydown", (event) => {
       switch (event.code) {
-        case 'ArrowUp': 
-          console.log('testUp');
-          this.chihuahua.moveUp()
+        case "ArrowUp":
+          console.log("testUp");
+          this.chihuahua.moveUp();
           break;
-        case 'ArrowDown':
-          console.log('testDown'); 
-          this.chihuahua.moveDown()
+        case "ArrowDown":
+          console.log("testDown");
+          this.chihuahua.moveDown();
           break;
-        case 'ArrowLeft':
-          console.log('testLeft'); 
-          this.chihuahua.moveLeft()
+        case "ArrowLeft":
+          console.log("testLeft");
+          this.chihuahua.moveLeft();
           break;
-        case 'ArrowRight':
-          console.log('testRight'); 
-          this.chihuahua.moveRight()
+        case "ArrowRight":
+          console.log("testRight");
+          this.chihuahua.moveRight();
           break;
       }
     });
-
-   }
+  }
 } //close Game class
-
-
-
-
--- js/obstacles.js
+++ js/obstacles.js
@@ -1,64 +1,69 @@
 class bag {
-    constructor(ctx) {
-        this.ctx = ctx
+  constructor(ctx) {
+    this.ctx = ctx;
 
-        this.x = 900;
-        this.y = 300;
-        this.width = 300;
-        this.height = 200;
-
- 
-    }
-    drawBag() {
-        this.ctx.fillStyle = 'brown';
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
+    this.x = 900;
+    this.y = 300;
+    this.width = 300;
+    this.height = 200;
+  }
+  drawBag() {
+    this.ctx.fillStyle = "brown";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
 
 class obstacle {
-    constructor(ctx, x, y, width, height) {
-      this.ctx = ctx
-      this.x = this.randomX()
-      this.y = 0
-      this.width = 150
-      this.height = 200
+  constructor(ctx, x, y, width, height) {
+    this.ctx = ctx;
+    this.x = this.randomX();
+    this.y = 0;
+    this.width = 150;
+    this.height = 200;
 
-      this.speedX = 0
-      this.speedY = 1
+    this.speedX = 0;
+    //this.speedY = 1
+    this.speedY = this.randomSpeed();
+  }
+  drawObstacle() {
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    this.y = this.y + this.speedY;
+    if (this.y > 800) {
+      this.y = this.y % 800;
+    } else if (this.y < 0) {
+      this.y = (this.y + 800) % 800;
     }
-    drawObstacle() {
-        this.ctx.fillStyle = 'black';  
-        this.ctx.fillRect(
-          this.x,
-          this.y,
-          this.width,
-          this.height
-        )
-      }
-     obstacleRandomFall() {
-    
-        this.x += this.speedX
-        this.y += this.speedY
-    }
+  }
 
-    //  obstacleRandomRise() {
-        
-    //     this.y = 800
-    //     this.x += this.speedX
-    //     this.y -= this.speedY
-    // }
+  startMove() {
+    setInterval(this.move.bind(this));
+  }
 
-    randomX() {
-        return Math.random()*900
-    }  
-
-    createRandomObstacles() {
-        
-        this.game.RandomObstacles.push(new obstacle(ctx, this.randomX, 0, this.width, this.height)); //falling obstacle
-        this.game.RandomObstacles.push(new obstacle(ctx, this.randomX, this.canvas.height, this.width, this.height)); // rising obstacle
-    }
-}
+  obstacleRandomFall() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
 
  
 
-    
+  randomX() {
+    return Math.random() * 900;
+  }
+
+  randomSpeed() {
+    return 1 + Math.random() * (-1 - 1);
+  }
+  createRandomObstacles() {
+    //let temporaryObstacle = new obstacle(ctx, x, y, width, height)
+    //RandomObstacles.push(temporaryObstacle)
+    //RandomObstacles.push(new obstacle(ctx, x, y, width, height));
+    //     this.game.RandomObstacles.push(temporaryObstacle)
+    // this.game.RandomObstacles.push(new obstacle(ctx, x, y, width, height));
+    //     this.game.RandomObstacles.push(new obstacle(ctx, this.randomX, 0, this.width, this.height)); //falling obstacle
+    //     this.game.RandomObstacles.push(new obstacle(ctx, this.randomX, this.canvas.height, this.width, this.height)); // rising obstacle
+  }
+}
